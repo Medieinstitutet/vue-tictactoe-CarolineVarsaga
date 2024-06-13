1f8accade327b5d00667ebcae2ee291268a4e546
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-13 10:55:17 +0200

    :sparkles: adds LandingPage component

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+//import { ref } from 'vue'
+import AddPlayer from './AddPlayer.vue'
+
+</script>
+
+<template>
+  <h1>Tic Tac Toe</h1>
+  <h2>Enter player name</h2>
+  <AddPlayer />  
+</template>
+
+<style scoped>
+
+</style>
