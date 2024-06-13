331433370c4a41200d1e56580eff92da5aa472fd
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-13 14:17:15 +0200

    :sparkles: imports game field component

--- src/components/GameApp.vue
+++ src/components/GameApp.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
+  import GameField from './GameField.vue';
 //import { ref } from 'vue'
 
 </script>
 
 <template>
-<h1>Game App</h1>
+  <GameField /> 
+
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
