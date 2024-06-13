e6b2c818c19713b7ed6c9673076d1ff97606d919
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-13 14:47:21 +0200

    :sparkles: adds winning component with props

---
+++ src/components/WinningPlayer.vue
@@ -0,0 +1,20 @@
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{ winner: string | null }>();
+</script>
+
+<template>
+  <div v-if="winner" class="winner">
+    {{ winner }} wins!
+  </div>
+</template>
+
+<style scoped>
+  .winner {
+    margin-top: 20px;
+    font-size: 24px;
+    color: green;
+  }
+</style>
