b3e7bf9a7c39b21ec0fc20e6193c3fe30fcdee10
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-13 13:22:17 +0200

    :sparkles: if else to display components

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
-//import { ref } from 'vue'
-import AddPlayer from './AddPlayer.vue'
+import { ref } from 'vue'
+import AddPlayer from './AddPlayer.vue'; 
+import GameApp from './GameApp.vue'; 
 
+const playerX = ref<string>(localStorage.getItem('playerX') || '');
+const playerO = ref<string>(localStorage.getItem('playerO') || '');
+
+const handlePlayerSaved = () => {
+  playerX.value = localStorage.getItem('playerX') || '';
+  playerO.value = localStorage.getItem('playerO') || '';
+}; 
 </script>
 
 <template>
   <h1>Tic Tac Toe</h1>
-  <h2>Enter player name</h2>
-  <AddPlayer />  
+  <div>
+    <AddPlayer v-if="!playerX || !playerO" @playerSaved="handlePlayerSaved" />
+    <GameApp v-else />
+  </div>
 </template>
 
 <style scoped>
