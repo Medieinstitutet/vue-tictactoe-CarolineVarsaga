1b8d39daf34939403f88c86e317603c14e592916
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-14 15:01:02 +0200

    :sparkles: adds button component

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+  const props = defineProps({
+    onClick: Function,
+  });
+
+  const handleClick = () => {
+    if (props.onClick) {
+      props.onClick();
+    }
+  };
+</script>
+
+<template>
+  <button @click="handleClick">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped></style>
