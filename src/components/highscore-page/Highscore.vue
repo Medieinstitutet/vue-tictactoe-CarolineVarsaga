f679a2ce6fe1f1ef49cea5fe8b9a89a32223f3e5
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-14 10:47:48 +0200

    🚧 adds highscore component

---
+++ src/components/highscore-page/Highscore.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="highscore-container">
+    <h2>Highscore</h2>  
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Points</th>
+      </tr>
+      <tr>
+        <td>Caroline</td>
+        <td>0</td>
+      </tr>
+      <tr>
+        <td>T</td>
+        <td>2</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+  .highscore-container {
+    display: flex; 
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  tr {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr); 
+    gap: 5rem; 
+  }
+</style>
