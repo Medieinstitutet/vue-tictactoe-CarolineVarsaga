7a6a05b7ca144ba31041674b969867dc11f8bc9e
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-17 10:08:50 +0200

    :sparkles: updates props

---
+++ src/components/game-board/Cell.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+  interface ICellProps {
+    key: number; 
+    cell: string;
+    index: number; 
+    onClick: (index: number) => void; 
+  }
+
+  const props = defineProps<ICellProps>(); 
+
+  const handleClick = () => {
+    props.onClick(props.index);
+  };  
+
+</script>
+
+<template>
+  <div class="cell" @click="handleClick">
+    {{ props.cell }}
+  </div>
+</template>
+
+<style scoped>
+.cell {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #ccc;
+  height: 100px;
+  font-size: 5rem;
+  cursor: pointer;
+}
+</style>
