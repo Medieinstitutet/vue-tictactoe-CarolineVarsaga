4350ecf720a0be786a9191c23e3150565329ec04
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-06-17 11:41:21 +0200

    :sparkles: imports game field

--- src/components/landing-page/LandingPage.vue
+++ src/components/landing-page/LandingPage.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
-import { ref } from "vue"
-import AddPlayer from "./AddPlayer.vue"; 
-import GameApp from "../../components/game-board/GameApp.vue";
+  import { ref } from "vue"
+  import AddPlayer from "./AddPlayer.vue"; 
+  import GameField from "../../components/game-board/GameField.vue";
 
-const playerX = ref<string>(localStorage.getItem("playerX") || "");
-const playerO = ref<string>(localStorage.getItem("playerO") || "");
+  const playerX = ref<string>(localStorage.getItem("playerX") || "");
+  const playerO = ref<string>(localStorage.getItem("playerO") || "");
 
-const handlePlayerSaved = () => {
-  playerX.value = localStorage.getItem("playerX") || "";
-  playerO.value = localStorage.getItem("playerO") || "";
-}; 
+  const handlePlayerSaved = () => {
+    playerX.value = localStorage.getItem("playerX") || "";
+    playerO.value = localStorage.getItem("playerO") || "";
+  }; 
+
 </script>
 
 <template>
   <div>
-    <AddPlayer v-if="!playerX || !playerO" @playerSaved="handlePlayerSaved" />
-    <GameApp v-else />
+    <AddPlayer 
+      v-if="!playerX || !playerO" 
+      @playerSaved="handlePlayerSaved" 
+      :playerX="playerX"
+      :playerO="playerO"
+    />
+    <GameField v-else 
+      :playerX="playerX"
+      :playerO="playerO"
+    />
   </div>
 </template>
 
